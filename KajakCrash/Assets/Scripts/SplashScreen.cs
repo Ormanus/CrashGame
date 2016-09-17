16691549a92259cc9d7e614060b0f0ced005ca0f
sakuronkko <saku.ronkko@hotmail.fi>
2016-09-18 00:41:29 +0300

    Animations

---
+++ KajakCrash/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    public GameObject canvas;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 8.0f;
+        canvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            canvas.SetActive(true);
+        }
+	}
+}
