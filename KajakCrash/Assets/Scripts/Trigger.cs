bc8d1356a381343d09e2d9dd43a2e09970953f75
joonaoikarinen <joonaoikarinen@kamk.fi>
2016-09-17 19:36:45 +0300

    Add files via upload
    "paranneltu" trigger.cs

--- KajakCrash/Assets/Scripts/Trigger.cs
+++ KajakCrash/Assets/Scripts/Trigger.cs
@@ -6,23 +6,34 @@ public class Trigger : MonoBehaviour
 {
 
     public Text name_text;
-
-    private string testName = "Taman Pitaisi Nakua";
+    bool playerIsNear;
 
     void start()
     {
-        name_text.text = "";
+        playerIsNear = false;
+        name_text.text = "teksti toimii";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerIsNear)
+        {
+            //Debug.Log("Spaaaace!");
+            gameObject.GetComponent<Computer>().state = ComputerState.Off;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(gameObject.name + testName);
-        //other.gameObject.SetActive(false);
-        name_text.text = "Do YOU see this????";
+        name_text.text = "space available";
+        playerIsNear = true;
+
+        
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         name_text.text = "";
+        playerIsNear = false;
     }
 }
