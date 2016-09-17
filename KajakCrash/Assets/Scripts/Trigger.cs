32ddd4185c169daf767f0ec4a8943134f0ec156f
Secrito45 <konsta.siekkeli@gmail.com>
2016-09-17 13:12:56 +0300

    new Prefabs, prefabs editing and one script

---
+++ KajakCrash/Assets/Scripts/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour
+{
+
+    public Text name_text;
+
+    private string testName = "Taman Pitaisi Nakua";
+
+    void start()
+    {
+        name_text.text = "";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(gameObject.name + testName);
+        //other.gameObject.SetActive(false);
+        name_text.text = "Do YOU see this????";
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        name_text.text = "";
+    }
+}
